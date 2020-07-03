bf81ced2f499bcc86b5160d064da484d09258e64
yenilikci <melihcelik017@outlook.com>
2020-07-03 16:57:32 +0300

    Kurların Hesaplanması

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const api = "https://api.exchangeratesapi.io/";
 //Elementlerin Seçimi
 const el_currency_one = document.getElementById('currency_one');
 const el_currency_two = document.getElementById('currency_two');
-const amount = document.getElementById('amount');
+const el_amount = document.getElementById('amount');
 const el_btn_calculate = document.getElementById('btn_calculate');
 const el_result = document.getElementById('result');
 
@@ -27,9 +27,26 @@ fetch('./birim.json')
 
     }
 
-
-    el_currency_one.innerHTML += options; 
-    el_currency_two.innerHTML += options;
-    
+    el_currency_one.innerHTML += options; //seçenekler eklendi ilk kur'a
+    el_currency_two.innerHTML += options; //seçenekler eklendi ikinci kur'a
     
 }) 
+
+el_btn_calculate.addEventListener('click',function(){
+    const base_currency = el_currency_one.value; //esas alınan ilk kur anahtar değeri
+    const to = el_currency_two.value; //çevrilecek olan ikinci kur anahtar değeri
+    const amount = el_amount.value; //miktar değeri
+
+
+    fetch(`${api}latest?base=${base_currency}`)
+    .then(res => res.json())
+    .then(data => {
+        const rate = data.rates[to];
+        el_result.className = 'alert alert-success p-2 text-center mt-3';
+        el_result.innerHTML = `${amount} ${base_currency} = ${amount * rate} ${to}`
+    })
+    el_result.style.display = 'block';
+
+    console.log(el_currency_one.value);
+    
+});
